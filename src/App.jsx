bd853ffd4e007044975db85140f2c923166594e7
Wiacek <benjamin.wiacek@gmail.com>
2026-01-14 16:09:29 -0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AssemblyEndgame() {
+    return (
+        <main>
+            <header>
+				<h1>Assembly: Endgame</h1>
+				<p className="description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+        </main>
+    )
+}
